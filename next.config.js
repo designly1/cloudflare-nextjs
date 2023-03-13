4a2728cd93dca9411481b5c6d1f901c2164862ff
Jay Simons <jay@designly.biz>
2023-03-13 17:02:26 -0500

    init

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: 'custom',
-    loaderFile: './cfImageLoader.js'
-  },
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      swcMinify: true,
+    }
+  }
+
+  return {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      loader: 'custom',
+      loaderFile: './cfImageLoader.js'
+    },
+  }
+}
---
+++ src/pages/api/products.js
@@ -0,0 +1,36 @@
+// @/pages/api/products.js
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
+
+// Tell Next.js to use the edge runtime
+export const config = {
+  runtime: 'edge',
+}
+
+export default async function handler(req) { // notice we don't receive a 'res' prop
+  const url = "https://dummyjson.com/products";
+
+  try {
+    const result = await fetch(url);
+    const json = await result.json();
+
+    // Instead of the 'res' ojbect from express, we instantiate a new fetch API Response object
+    return new Response(
+      JSON.stringify(json),
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+  } catch (err) {
+    console.error(err);
+    return new Response(
+      null,
+      {
+        status: 500,
+        statusText: 'Error fetching product data'
+      }
+    )
+  }
+}
